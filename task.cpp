1399d050ae310472f0888b48efa3d65ac23c8d86
Jiří Šebele <sindriava@gmail.com>
2020-05-14 21:25:57 +0200

    Update task parallelism implementation

--- task.cpp
+++ task.cpp
@@ -8,42 +8,23 @@
 #include "Problem.hpp"
 #include "Util.hpp"
 
+constexpr size_t THRESHOLD = 10;
+
 using VSolution = std::vector<uint8_t>;
-
-struct Result {
-    VSolution solution;
-    float weight = std::numeric_limits<float>::infinity();
-};
-
-bool operator<(const Result& a, const Result& b) {
-    return a.weight < b.weight;
-}
 
 Problem problem;
 
+VSolution bestSolution;
 float bestWeight = std::numeric_limits<float>::infinity();
-
-int32_t invert(int32_t group) {
-    if (group == 1) {
-        return 2;
-    }
-    else if (group == 2) {
-        return 1;
-    }
-    else {
-        assert(false);
-    }
-}
 
 std::vector<std::vector<Edge>> bedges;
 
-[[nodiscard]]
-Result solve(int pos, VSolution solution, float weight) {
+void solve(int pos, VSolution solution, float weight) {
     assert(pos > 0);
 
     // Satisfy exclusions
     if (problem.exclusions.count(pos - 1) > 0) {
-        solution[problem.exclusions.at(pos - 1)] = invert(solution[pos - 1]);
+        solution[problem.exclusions.at(pos - 1)] = Util::invert(solution[pos - 1]);
     }
 
     // Calculate the weight
@@ -55,74 +36,72 @@ Result solve(int pos, VSolution solution, float weight) {
 
     // Can't do better
     if (bestWeight < weight) {
-        return { solution, std::numeric_limits<float>::infinity() };
+        return;
     }
 
     if (pos == problem.n) {
         if (bestWeight > weight) {
-            bestWeight = weight;
+            #pragma omp critical
+            {
+                if (bestWeight > weight) {
+                    bestSolution = solution;
+                    bestWeight = weight;
+                }
+            }
         }
-        return { solution, weight };
+        return;
     }
 
     // Value already set
     if (solution[pos] != 0) {
-        return solve(pos + 1, solution, weight);
+        return solve(pos + 1, std::move(solution), weight);
     }
 
     // Recurse
-    Result result1, result2;
+    #pragma omp task if (pos < problem.n - THRESHOLD)
+    {
+        solution[pos] = 1;
+        solve(pos + 1, solution, weight);
+    }
 
-    solution[pos] = 1;
-#pragma omp task shared(result1) if (pos < problem.n - 10)
-    result1 = solve(pos + 1, solution, weight);
-
-    solution[pos] = 2;
-#pragma omp task shared(result2) if (pos < problem.n - 10)
-    result2 = solve(pos + 1, solution, weight);
-
-#pragma omp taskwait
-
-    return std::min(result1, result2);
+    #pragma omp task if (pos < problem.n - THRESHOLD)
+    {
+        solution[pos] = 2;
+        solve(pos + 1, std::move(solution), weight);
+    }
 }
 
-[[nodiscard]]
-Result solve() {
-    VSolution solution;
-    solution.resize(problem.n);
-    solution[0] = 1;
+int main(int argc, const char** argv) {
+    // Load data
+    problem = Problem::load(argc, argv);
 
+    // Calculate bedges
     bedges.resize(problem.n);
     for (auto& e : problem.edges) {
         auto [a, b, v] = e;
         bedges[b].push_back(e);
     }
 
-    return solve(1, solution, 0.0f);
-}
-
-int main(int argc, const char** argv) {
-    /* Load data */
-    problem = Problem::load(argc, argv);
-
-    /* Solve problem */
-    Result result;
-
+    // Solve problem
     auto elapsed_time = timed {
+        VSolution solution;
+        solution.resize(problem.n);
+        solution[0] = 1;
+
         #pragma omp parallel
         {
             #pragma omp single
             {
-                result = solve();
+                solve(1, solution, 0.0f);
             }
         }
     };
 
-    /* Print results */
+    // Print results
     printf("Variant: Task parallelism\n");
     printf("Problem: %s\n", problem.name.c_str());
-    printf_vector("Solution", result.solution);
-    printf("Weight: %f\n", result.weight);
+    printf_vector("Solution", bestSolution);
+    printf("Weight: %f\n", bestWeight);
     printf("Elapsed time: %3fs", elapsed_time.count());
 
     return 0;
